776020ab6171c611b8f27bf27e312bd8795d710f
moshiurimu <moshiurimu@gmail.com>
2014-09-29 16:54:14 +0600

    Name List App using ArrayList

---
+++ NameListApp/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace NameListApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.totalTextBox = new System.Windows.Forms.TextBox();
+            this.namesListBox = new System.Windows.Forms.ListBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.showAllNameButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // totalTextBox
+            // 
+            this.totalTextBox.Location = new System.Drawing.Point(193, 290);
+            this.totalTextBox.Name = "totalTextBox";
+            this.totalTextBox.Size = new System.Drawing.Size(100, 20);
+            this.totalTextBox.TabIndex = 10;
+            // 
+            // namesListBox
+            // 
+            this.namesListBox.FormattingEnabled = true;
+            this.namesListBox.Location = new System.Drawing.Point(94, 124);
+            this.namesListBox.Name = "namesListBox";
+            this.namesListBox.Size = new System.Drawing.Size(199, 160);
+            this.namesListBox.TabIndex = 9;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(94, 26);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(199, 20);
+            this.nameTextBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Enter Name";
+            // 
+            // showAllNameButton
+            // 
+            this.showAllNameButton.Location = new System.Drawing.Point(194, 90);
+            this.showAllNameButton.Name = "showAllNameButton";
+            this.showAllNameButton.Size = new System.Drawing.Size(99, 23);
+            this.showAllNameButton.TabIndex = 8;
+            this.showAllNameButton.Text = "Show All Name";
+            this.showAllNameButton.UseVisualStyleBackColor = true;
+            this.showAllNameButton.Click += new System.EventHandler(this.showAllNameButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(218, 55);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 6;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(91, 293);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Total Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 336);
+            this.Controls.Add(this.totalTextBox);
+            this.Controls.Add(this.namesListBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.showAllNameButton);
+            this.Controls.Add(this.addButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox totalTextBox;
+        private System.Windows.Forms.ListBox namesListBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button showAllNameButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ NameListApp/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NameListApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ArrayList names = new ArrayList();
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            names.Add(nameTextBox.Text);
+            nameTextBox.Text = "";
+        }
+
+        private void showAllNameButton_Click(object sender, EventArgs e)
+        {
+            namesListBox.Items.Clear();
+            foreach (string aName in names)
+            {
+                namesListBox.Items.Add(aName);
+            }
+            totalTextBox.Text = names.Count.ToString();
+        }
+    }
+}
